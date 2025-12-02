ea6738d8f94eb628ac1518aada26c39cd9f86ffb
Kwon IK Hwan <ksi9421@gmail.com>
2025-12-02 15:50:55 +0900

    Team02.Build.cs //  PublicIncludePaths "Team02" 추가 KIHActor 추가

--- Source/Team02/Team02.Build.cs
+++ Source/Team02/Team02.Build.cs
@@ -9,5 +9,7 @@ public class Team02 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PublicIncludePaths.AddRange(new string[] { "Team02" });
 	}
 }
